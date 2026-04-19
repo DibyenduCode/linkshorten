b1a76964acbae27fe3344e2c9964e94211d77c5f
Dibyendu <khaskeldibyendu22@gmail.com>
2026-04-20 02:17:11 +0530

    redir engin done

--- open.php
+++ open.php
@@ -1,4 +1,15 @@
 <?php
 $code= substr($_SERVER["REDIRECT_URL"],6);
-echo $code;
+
+$file="data.json";
+$filehandel=fopen($file,"r");
+$filejson=fread($filehandel,filesize($file));
+fclose($filehandel);
+$oldjsontoarray=json_decode($filejson,true);
+
+$originalurl= $oldjsontoarray["$code"];
+http_response_code(301);
+header("location: ".$originalurl);
+exit();
+        
 ?>
